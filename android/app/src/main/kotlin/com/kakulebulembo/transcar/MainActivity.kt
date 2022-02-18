707812dc3320b158d97487cef357289ed34a45d8
oliviasaina <ageofthinking101@gmail.com>
2022-02-18 10:30:55 +0300

    Transcar

---
+++ android/app/src/main/kotlin/com/kakulebulembo/transcar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kakulebulembo.transcar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xFF212332);
+const kPrimaryColor = Color(0xFF2A2D3E);
